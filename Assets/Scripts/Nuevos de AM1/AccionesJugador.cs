dbaf2709c9e810f18c003020976a4f9adf4580a4
DamianDavincih <damian.figueredo@davinci.edu.ar>
2025-05-09 16:26:38 -0300

    mmm

--- Assets/Scripts/Nuevos de AM1/AccionesJugador.cs
+++ Assets/Scripts/Nuevos de AM1/AccionesJugador.cs
@@ -62,10 +62,15 @@ public class AccionesJugador : A1_Entidad
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
         StartCoroutine(EsperarLlegada());
 =======
 >>>>>>> parent of 7b567d2 (cambios part 2)
+=======
+        float velocidad = new Vector3(GetComponent<Rigidbody>().velocity.x, 0, GetComponent<Rigidbody>().velocity.z).magnitude;
+        animator.SetFloat("velocidad", velocidad);
+>>>>>>> parent of f9429c8 (animaciones part 3)
 =======
         float velocidad = new Vector3(GetComponent<Rigidbody>().velocity.x, 0, GetComponent<Rigidbody>().velocity.z).magnitude;
         animator.SetFloat("velocidad", velocidad);
